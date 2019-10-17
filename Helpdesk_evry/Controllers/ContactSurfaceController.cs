a0d0a3387082232b65eba1ac69532dad24cbe5a2
antonebeling <anton.ebeling@evry.com>
2019-10-17 16:51:14 +0200

    Added Form and styleed it somewhat, also added some padding to certain objects to make it more user friendly

---
+++ Helpdesk_evry/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,38 @@
+﻿using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using Helpdesk_evry.Models;
+using System.Net.Mail;
+
+namespace Helpdesk_evry.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Contact/";
+
+        public ActionResult RenderForm()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Contact.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ContactModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                SendEmail(model);
+                return RedirectToCurrentUmbracoPage(); 
+            }
+            return CurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            MailMessage message = new MailMessage("support@evryhalmstad.se", "website@Helpdesk.evry.web.local");
+            message.Subject = string.Format("Enquiry from {0} {1} - {2}", model.FirstName, model.LastName, model.EmailAddress);
+            message.Body = model.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}

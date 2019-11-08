5a38370a23c41f66499e2dd4edb6382c1a2a7cb4
antonebeling <anton.ebeling@evry.com>
2019-11-08 14:58:19 +0100

    the login looks good and works towards umbraco, dosent link to anything useful tho

---
+++ Helpdesk_evry/Controllers/MemberController.cs
@@ -0,0 +1,57 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+using Helpdesk_evry.Models;
+
+namespace Helpdesk_evry.Controllers
+{
+    public class MemberController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Login/";
+
+        public ActionResult RenderLogin()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Login.cshtml", new LoginModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Username, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    UrlHelper myHelper = new UrlHelper(HttpContext.Request.RequestContext);
+                    if (myHelper.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return Redirect("/login/");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password provided is incorrect.");
+                }
+            }
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Logout.cshtml", null);
+        }
+
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
